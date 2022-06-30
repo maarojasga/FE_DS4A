9c3fc80aab161d193f060be17d345d4acfe67172
jcleivas <jcleivas@unal.edu.co>
2022-06-29 20:08:51 -0500

    Removing redundant code

--- apps/mapas.py
+++ apps/mapas.py
@@ -9,51 +9,13 @@ def app():
     plt.style.use('ggplot')
     plt.style.use('dark_background')
     #colors={:"green","":"purple","Anthro"}
-    color_discrete_map = {'Geophony': '#2E75B6', "Biophony": '#A9D18E', 'Anthrophony': '#FFD966',"Other":"#F4B183"}
+    color_discrete_map = {'geophony': '#2E75B6', "biophony": '#A9D18E', 'anthrophony': '#FFD966',"Other":"#F4B183"}
+    df = pd.read_csv('data/Labeled Metatabla.csv')
+    df['date'] = pd.to_datetime(df['date'])
 
-    def mapa():
-        fig2 = px.scatter_mapbox(df, lat="latitud", lon="longitud", hover_name="sensor_name", hover_data=["parent label", "time"],
-                        color_discrete_map=color_discrete_map,color="parent label", zoom=9, width=800)
-        fig2.update_layout(mapbox_style="open-street-map")
-
-        fig2.update_layout(margin={"r":0,"t":50,"l":0,"b":50})
-
-        fig2.update_coloraxes(showscale=False)
-        c1.plotly_chart(fig2)
-
-    def mapa_bio():
-        fig2 = px.scatter_mapbox(df_bio, lat="latitud", lon="longitud", hover_name="sensor_name", hover_data=["parent label", "parent label"],
-                        color_discrete_map=color_discrete_map,color="parent label", zoom=9, width=800)
-        fig2.update_layout(mapbox_style="open-street-map")
-
-        fig2.update_layout(margin={"r":0,"t":50,"l":0,"b":50})
-
-        fig2.update_coloraxes(showscale=False)
-        c1.plotly_chart(fig2)
-
-    def mapa_antro():
-        fig2 = px.scatter_mapbox(df_antro, lat="latitud", lon="longitud", hover_name="sensor_name", hover_data=["parent label", "parent label"],
-                        color_discrete_map=color_discrete_map, color="parent label", zoom=9, width=800)
-        fig2.update_layout(mapbox_style="open-street-map")
-
-        fig2.update_layout(margin={"r":0,"t":50,"l":0,"b":50})
-
-        fig2.update_coloraxes(showscale=False)
-        c1.plotly_chart(fig2)
-
-    def mapa_other():
-        fig2 = px.scatter_mapbox(df_other, lat="latitud", lon="longitud", hover_name="sensor_name", hover_data=["parent label", "parent label"],
-                        color_discrete_map=color_discrete_map,color="parent label", zoom=9, width=800)
-        fig2.update_layout(mapbox_style="open-street-map")
-
-        fig2.update_layout(margin={"r":0,"t":50,"l":0,"b":50})
-
-        fig2.update_coloraxes(showscale=False)
-        c1.plotly_chart(fig2)
-
-    def mapa_geo():
-        fig2 = px.scatter_mapbox(df_geo, lat="latitud", lon="longitud", hover_name="sensor_name", hover_data=["parent label", "parent label"],
-                        color_discrete_map=color_discrete_map, color="parent label", zoom=9, width=800)
+    def mapa(df):
+        fig2 = px.scatter_mapbox(df, lat="decimalLat", lon="decimalLon", hover_name="sensor_name", hover_data=["grand_label", "time"],
+                        color_discrete_map=color_discrete_map,color="grand_label", zoom=9, width=800)
         fig2.update_layout(mapbox_style="open-street-map")
 
         fig2.update_layout(margin={"r":0,"t":50,"l":0,"b":50})
@@ -63,35 +25,26 @@ def app():
 
     @st.cache
     def leer_data():
-        df = pd.read_csv('data/FrontEnd_Part1.csv')
-        df['Fecha'] = pd.to_datetime(df['Fecha'])
         return df
 
     def transf_bio():
-        df = pd.read_csv('data/FrontEnd_Part1.csv')
-        df['Fecha'] = pd.to_datetime(df['Fecha'])
-        df_bio = df[df['parent label'] == 'Biophony'] 
+        df_bio = df[df['grand_label'] == 'biophony'] 
+        print(df_bio.head())
         return df_bio
 
     def transf_antro():
-        df = pd.read_csv('data/FrontEnd_Part1.csv')
-        df['Fecha'] = pd.to_datetime(df['Fecha'])
-        df_antro = df[df['parent label'] == 'Anthrophony'] 
+        df_antro = df[df['grand_label'] == 'anthrophony'] 
         return df_antro
     
     def transf_other():
-        df = pd.read_csv('data/FrontEnd_Part1.csv')
-        df['Fecha'] = pd.to_datetime(df['Fecha'])
-        df_other = df[df['parent label'] == 'Other'] 
+        df_other = df[df['grand_label'] == 'Other'] 
         return df_other
 
     def transf_geo():
-        df = pd.read_csv('data/FrontEnd_Part1.csv')
-        df['Fecha'] = pd.to_datetime(df['Fecha'])
-        df_geo = df[df['parent label'] == 'Geophony'] 
+        df_geo = df[df['grand_label'] == 'geophony'] 
         return df_geo
 
-    with st.spinner('En proceso...'):
+    with st.spinner('Processing...'):
        df = leer_data()
        df_bio = transf_bio()
        df_antro = transf_antro()
@@ -108,22 +61,20 @@ def app():
     fecha_inicial = c1.date_input('Start date', datetime.date(2021, 11, 1))
     fecha_final = c1.date_input('End date', datetime.date(2021, 12, 30))
     c2.write('### Category')
-    tipo = c2.radio( 'Tipo', ['All']+list(set(df['parent label'])))
+    tipo = c2.radio( 'Tipo', ['All']+list(set(df['grand_label'])))
     #c3.write('### Sensor')
     #sensor = c3.multiselect('Nombre del sensor', ['Seleccionar']+list(set(df['name'])))
     seguir = c1.checkbox('Filter')
     st.write('--------------')
 
     if seguir:
-        if tipo == 'Biophony':
-            mapa_bio()
-        elif tipo == 'Anthrophony':
-            mapa_antro()
-        elif tipo == 'Geophony':
-            mapa_geo()
+        if tipo == 'biophony':
+            mapa(df_bio)
+        elif tipo == 'anthrophony':
+            mapa(df_antro)
+        elif tipo == 'geophony':
+            mapa(df_geo)
         elif tipo == 'Other':
-            mapa_other()
+            mapa(df_other)
         else:
-            mapa()
-        
-        #totales = data.sum()
+            mapa(df)
